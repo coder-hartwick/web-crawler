3b9983e2253cc122fafc5524e8a70b761f8675e3
Jordan Hartwick <programmerhartwick@programmer.net>
2016-05-14 22:24:22 -0500

    Created Main Window
    I added a JFrame and a JPanel. You now see a blank window, 800 x 600, with
    the title 'Web Crawler'.

--- src/web/crawler/WebCrawler.java
+++ src/web/crawler/WebCrawler.java
@@ -1,10 +1,13 @@
 package web.crawler;
 
+import java.awt.EventQueue;
+import web.crawler.gui.MainGraphics;
+
 
 /**
- * This is a web crawling application that uses the Jsoup libraries. It has a 
+ * This is a web crawling application that uses the Jsoup libraries. It has a
  * GUI written with Java Swing, which will display to the user information about
- * what the program is doing. This GUI is also where the user will tell the 
+ * what the program is doing. This GUI is also where the user will tell the
  * application what to do.
  *
  * @author  Jordan Hartwick
@@ -15,11 +18,17 @@ public class WebCrawler {
 
     /**
      * Main method of the program. Starts the GUI.
-     * 
+     *
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
-    }
+        EventQueue.invokeLater(new Runnable() {
 
+            @Override
+            public void run() {
+                new MainGraphics().createAndShowGUI();
+            }
+
+        });
+    }
 }
---
+++ src/web/crawler/gui/MainGraphics.java
@@ -0,0 +1,60 @@
+package web.crawler.gui;
+
+import java.awt.BorderLayout;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+
+/**
+ * This is the class that creates the main screen that the user sees. This
+ * screen will contain controls that the user will use to manage the web
+ * crawler.
+ *
+ * @author Jordan Hartwick
+ * May 14, 2016
+ */
+public class MainGraphics {
+
+
+    /** The main frame that holds the main panel in the application. */
+    private JFrame      windowFrame;
+
+
+    /** The main panel that holds graphical components in the application. */
+    private JPanel      windowPanel;
+
+
+    /**
+     * The MainGraphics constructor method will instantiate the classes needed
+     * to carry out web crawling operations.
+     */
+    public MainGraphics() {
+        // Instantiate classes in here.
+    }
+
+
+    /**
+     * Creates and shows the GUI to the user.
+     */
+    public void createAndShowGUI() {
+        windowFrame = new JFrame("Web Crawler");
+        windowPanel = new JPanel(new BorderLayout());
+
+        windowFrame.addWindowListener(new WindowAdapter() {
+
+            @Override
+            public void windowClosed(WindowEvent e) {
+                System.exit(0);
+            }
+        });
+
+        windowFrame.setContentPane(windowPanel);
+        windowFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        windowFrame.setSize(800, 600);
+        windowFrame.setLocationRelativeTo(null);
+        windowFrame.setVisible(true);
+    }
+
+}
