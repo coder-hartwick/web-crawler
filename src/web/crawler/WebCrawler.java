952b4be37c5a4609da26ab6f476b31c044e87d57
Jordan Hartwick <programmerhartwick@programmer.net>
2016-05-14 21:53:37 -0500

    New Commit
    Added the main class that will start the application.

---
+++ src/web/crawler/WebCrawler.java
@@ -0,0 +1,25 @@
+package web.crawler;
+
+
+/**
+ * This is a web crawling application that uses the Jsoup libraries. It has a 
+ * GUI written with Java Swing, which will display to the user information about
+ * what the program is doing. This GUI is also where the user will tell the 
+ * application what to do.
+ *
+ * @author  Jordan Hartwick
+ * May 14, 2016
+ */
+public class WebCrawler {
+
+
+    /**
+     * Main method of the program. Starts the GUI.
+     * 
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }
+
+}
